0c0301bc767288a1fd1fdde03688a4bc93b3049d
patosabogal <pato.sabogal@hotmail.com>
2017-11-21 14:11:42 -0300

    Agregamos shell interactivo

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -2,12 +2,12 @@ package service
 
 var tweet string
 
-// PublishTweet Publicar tweet
+// PublishTweet - Publicar tweet
 func PublishTweet(tw string) {
 	tweet = tw
 }
 
-// GetTweet Devuelve tweet
+// GetTweet - Devuelve tweet
 func GetTweet() string {
 	return tweet
 }
