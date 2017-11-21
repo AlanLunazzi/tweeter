3a069cdd35477d3c6a7a7b6df95802fdc2a5d2ba
patosabogal <pato.sabogal@hotmail.com>
2017-11-21 13:38:06 -0300

    IDE no rompe las bolas

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -1,12 +1,13 @@
 package service
 
-var tweet string = ""
+var tweet string
 
+// PublishTweet Publicar tweet
 func PublishTweet(tw string) {
 	tweet = tw
-
 }
 
+// GetTweet Devuelve tweet
 func GetTweet() string {
 	return tweet
 }
